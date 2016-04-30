b1be8564c0d6d8d26f2543a645b8435d2140f54a
Daniel Zaoui <daniel.zaoui@yahoo.com>
2016-04-30 17:50:15 +0300

    make: return an error result on bad compilation

--- make.sh
+++ make.sh
@@ -1,10 +1,12 @@
-cd src
-gcc -fPIC -c e_mod_main.c $CFLAGS `pkg-config --cflags enlightenment elementary` -o e_mod_main.o
-gcc -shared -fPIC -DPIC e_mod_main.o `pkg-config --libs enlightenment elementary` -Wl,-soname -Wl,module.so -o module.so
-cd -
+gcc -fPIC -c src/e_mod_main.c $CFLAGS `pkg-config --cflags enlightenment elementary` -o src/e_mod_main.o
+[ $? -eq 0 ] || exit 1
+gcc -shared -fPIC -DPIC src/e_mod_main.o `pkg-config --libs enlightenment elementary` -Wl,-soname -Wl,module.so -o src/module.so
+[ $? -eq 0 ] || exit 1
 
 /opt/e/bin/edje_cc -v -id ./images e-module-transmission.edc e-module-transmission.edj
+[ $? -eq 0 ] || exit 1
 /opt/e/bin/edje_cc -v -id ./images transmission.edc transmission.edj
+[ $? -eq 0 ] || exit 1
 
 release=$(pkg-config --variable=release enlightenment)
 host_cpu=$(uname -m)
